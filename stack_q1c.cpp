1ba5f890c6b66f34203dda5119f80078b4f38a23
Ashwinak2000 <42172715+Ashwinak2000@users.noreply.github.com>
2019-02-09 08:32:21 +0530

    Update stack_q1c.cpp

--- stack_q1c.cpp
+++ stack_q1c.cpp
@@ -1,44 +1,65 @@
 #include<iostream>
 using namespace std;
 
-void push(int stack[],int& top,int value)
+class stackArr
 {
- int size=10;
- if(top>=size)
- cout<<"stack overflow"<<endl;
- else
- {
-  top++;
-  stack[top]=value;
- }
-}
-void pop(int stack[],int& top)
-{
- if(top==-1)
- cout<<"stack underflow";
- else
- {
-  cout<<stack[top]<<":deleted"<<endl;
-  top--;
- }
-}
+ int stack[10];
+ int top;
+ public: stackArr()
+        {
+         //Intialize the elements
+         top=-1;
+         for(int i=0;i<10;i++)
+          stack[i]=0;
+        }
+       
+        void pop()
+        { //check if the stack is non empty
+          if(top!=-1)
+         {//delete the top most element
+          cout<<"Deleting:"<<stack[top]<<endl;
+          top--;
+         }
+         else
+         cout<<"Stack underflow"<<endl;//else display error message
+        }
+       
+        void push(int value)
+        {
+         int size=sizeof(stack)/sizeof(stack[0]);
+         //check if stack is full(overflowing)
+          if(top>size)
+          cout<<"Stack Overflow"<<endl;//If yes display error
+          else
+         {
+          top++;
+          stack[top]=value; //Else add a new element
+         }
+        }
 
-void display(int stack[],int& top)
-{
- for(int i=top;i>=0;i--)
- cout<<" "<<stack[i]<<" ";
- cout<<endl;
-}
+        void display()
+         {
+          for(int i=top;i>=0;i--)
+          cout<<stack[i]<<" ";//Display stack elements
+          cout<<endl;
+
+         }
+       
+};
+
+
 int main()
 {
- int arr[10];
- int top=-1;
- for(int i=0;i<10;i++)
- push(arr,top,2*i+1);
- display(arr,top);
- for(int i=0;i<10;i++)
- {
- 	pop(arr,top);
- }	
+stackArr s;
+s.push(1);
+s.display();
+s.push(2);
+s.display();
+s.pop();
+s.display();
+s.pop();
+s.display();
+s.pop();
+s.display();
  
 }
