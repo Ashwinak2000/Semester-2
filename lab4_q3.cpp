fb0021b2047c7b423a0b6eb85f741c48181b6190
Ashwinak2000 <42172715+Ashwinak2000@users.noreply.github.com>
2019-02-10 21:01:01 +0530

    Update lab4_q3.cpp

--- lab4_q3.cpp
+++ lab4_q3.cpp
@@ -1,79 +1,98 @@
 #include<iostream>
 using namespace std;
+// A linked list (LL) node to store a queue entry 
+class node 
+{ 
+    public:
+	int data; 
+	node *next; 
+	node()
+	{
+		data=0;
+		next=NULL;
+	}
+}; 
 
-//Converts a queue into a stack array 
-class QueueArr
-{
- int queue[100];
- int front;
- int tail;
- public: QueueArr()
+// The queue, front stores the front node of LL and rear stores ths 
+// last node of LL 
+class Queue 
+{ 
+  node *front, *rear; 
+ public:
+        Queue()
         {
-         //Intialize the elements
-         tail=front=-1;
-         for(int i=0;i<100;i++)
-          queue[i]=0;
-        }
-      
-        int dequeue()
-        { //check if the queue is non empty
-          if(front!=tail&&front>=0)
-         {//delete the top most element
-          int t=queue[front];
-          cout<<"Deleting:"<<queue[front]<<endl;
-          front++;
-          return t;
-         }
-         else
-         cout<<"Queue underflow"<<endl;//else display error message
-        }
-      
-        void enqueue(int value)
-        {int size=sizeof(queue)/sizeof(queue[0]);
-         if(front==-1)
-         {
-          front=tail=0;
-          queue[front]=value;
-         }
-         if(tail<size)
-         {
-          queue[tail]=value;
-          tail++;
-         }
-        }
+         front=NULL;
+         rear=NULL;
+		}
+        
+// The function to add a data k to q 
+void enQueue(int k) 
+{  
+    node* temp=new node;
+    temp->data=k;
+	// If queue is empty, then new node is front and rear both 
+	if (front == NULL) 
+	{ 
+	 front=temp; 
+	 rear=temp; 
+	} 
+	else// Add the new node at the end of queue and change rear 
+	{
+	 rear->next=temp; 
+	 rear=temp; 
+    }
+} 
 
-        void display()
-         {
-          for(int i=front;i<tail;i++)
-          cout<<queue[i]<<" ";//Display queue elements
-          cout<<endl;
-         }
-        int size(int value)
-       {
-        if(value==0)
-        return sizeof(queue)/sizeof(queue[0]);
-        else
-        return tail-front-1;
-       }
-      
-       int is_empty()
-       {
-         if(front==tail&&front==-1)
-          return 1;
-         else
-          return 0;
-       }
-};
+// Function to remove a key from given queue q 
+int deQueue() 
+{ 
+	// If queue is empty, return NULL. 
+	if (front==NULL) 
+	cout<<"Queue is empty"; 
 
+	// Store previous front and move front one node ahead 
+    node *temp=front; 
+	front=front->next; 
+
+	// If front becomes NULL, then change rear also as NULL 
+	if (front==NULL) 
+	rear=NULL; 
+	cout<<temp->data<<":deleted"<<endl;
+	return temp->data;
+	 
+} 
+
+void display()
+{
+	node* temp=front;
+	while(temp!=NULL)
+	{
+	 cout<<temp->data<<" ";
+	 temp=temp->next;
+	}
+	cout<<endl;
+}
+
+int size()
+{int size;
+ node* temp=front;
+ while(temp!=NULL)
+ {
+  size++;
+  temp=temp->next;
+ } 
+ return size;
+}
+}; 
 class stack
 {
- QueueArr q;
+ Queue q;
  public: void push(int value)
         {
-         q.enqueue(value);
-         int size=q.size(1);
-         for(int i=0;i<size;i++)
-          q.enqueue(q.dequeue());
+         q.enQueue(value);
+         int size=q.size();
+         for(int i=0;i<1;i++)
+          q.enQueue(q.deQueue());
         }
          void display()
         {
@@ -81,7 +100,7 @@ class stack
         }
         void pop()
        {
-         q.dequeue();
+         q.deQueue();
        }
 };
 
@@ -90,7 +109,7 @@ int main()
  stack s;
  s.push(1);
  s.display();
-  s.push(2);
+ s.push(2);
  s.display();
  s.push(3);
  s.display();
